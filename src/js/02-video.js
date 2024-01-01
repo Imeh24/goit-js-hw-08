7f9176a39dbf09753b288af46019afe6464ca8a6
Imeh24 <medinajeremymarcel@gmail.com>
2024-01-01 22:04:14 +0800

    Add code for task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
 
+
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iFrame = document.querySelector('#vimeo-player');
+const player = new Player(iFrame);
+
+
+player.on('timeupdate', throttle(handleThrottle, 1000));
+
+function handleThrottle (data) {
+    const time = data.seconds;
+    console.log(time);
+    localStorage.setItem("videoplayer-current-time", time);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the gitvideo’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
