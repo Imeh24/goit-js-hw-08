761f601d6e13075915efd7cb7cc783555bc9879b
Imeh24 <medinajeremymarcel@gmail.com>
2024-01-03 22:06:58 +0800

    add codes for task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const localKey = 'feedback-form-state';
+
+
+
+const updateLocalStorageThrottled = throttle(() => {
+  const data = { email: form.email.value, message: form.message.value };
+  localStorage.setItem(localKey, JSON.stringify(data));
+}, 500);
+
+form.addEventListener('input', () => {
+  updateLocalStorageThrottled();
+});
+
+
+
+const storedData = localStorage.getItem(localKey);
+const { email = '', message = '' } = storedData ? JSON.parse(storedData) : {};
+
+form.message.value = message;
+form.message.placeholder = 'Write your feedback';
+form.email.value = email;
+form.email.placeholder = 'Valid email address';
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  if (storedData) {
+    try {
+      const { email, message } = JSON.parse(storedData);
+      console.log('Feedback submitted:');
+      console.log('Email:', email);
+      console.log('Message:', message);
+      console.log({ email, message });
+    } catch (error) {
+      console.error('Error parsing stored data:', error);
+    }
+  }
+
+  localStorage.removeItem(localKey);
+  form.reset();
+});
