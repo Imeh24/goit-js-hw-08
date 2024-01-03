73bbbe3f199288c3cd50c72c43aadc73f59f199d
Imeh24 <medinajeremymarcel@gmail.com>
2024-01-03 22:19:20 +0800

    update codes

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,31 +3,27 @@ import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
 const localKey = 'feedback-form-state';
 
-
-
-const updateLocalStorageThrottled = throttle(() => {
+function updateLocalStorageThrottled() {
   const data = { email: form.email.value, message: form.message.value };
   localStorage.setItem(localKey, JSON.stringify(data));
-}, 500);
+}
 
-form.addEventListener('input', () => {
-  updateLocalStorageThrottled();
+const throttledUpdateLocalStorage = throttle(updateLocalStorageThrottled, 500);
+
+form.addEventListener('input', function() {
+  throttledUpdateLocalStorage();
 });
-
-
 
 const storedData = localStorage.getItem(localKey);
 const { email = '', message = '' } = storedData ? JSON.parse(storedData) : {};
-
 
 form.email.value = email;
 form.email.placeholder = 'Valid email address';
 form.message.value = message;
 form.message.placeholder = 'Write your feedback';
 
-form.addEventListener('submit', (event) => {
+form.addEventListener('submit', function(event) {
   event.preventDefault();
-
   if (storedData) {
     try {
       const { email, message } = JSON.parse(storedData);
